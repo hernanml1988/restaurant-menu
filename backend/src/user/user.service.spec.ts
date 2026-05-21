542a7b2ff49833134cedcc40418bb0d289c37f59
hernanml1988 <hernanm.l.1988@gmail.com>
2026-05-21 15:29:35 -0400

    feat: add Restaurant and Subscription services to UserService tests

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -2,7 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Profile } from '../profile/entities/profile.entity';
 import { ProfileRole } from '../profile_role/entities/profile_role.entity';
+import { Restaurant } from '../restaurant/entities/restaurant.entity';
+import { RestaurantService } from '../restaurant/restaurant.service';
+import { RestaurantStaff } from '../restaurant_staff/entities/restaurant_staff.entity';
 import { Role } from '../role/entities/role.entity';
+import { SubscriptionService } from '../subscription/subscription.service';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 
@@ -17,6 +21,10 @@ describe('UserService', () => {
         { provide: getRepositoryToken(Role), useValue: {} },
         { provide: getRepositoryToken(Profile), useValue: {} },
         { provide: getRepositoryToken(ProfileRole), useValue: {} },
+        { provide: getRepositoryToken(Restaurant), useValue: {} },
+        { provide: getRepositoryToken(RestaurantStaff), useValue: {} },
+        { provide: RestaurantService, useValue: {} },
+        { provide: SubscriptionService, useValue: {} },
       ],
     }).compile();
 
